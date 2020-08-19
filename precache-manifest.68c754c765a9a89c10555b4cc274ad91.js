d37629e204595ec890694dbe250f7fc09966dc81
Karin Hsu <khsu0720@gmail.com>
2020-08-20 00:15:40 +0800

    Updates

---
+++ precache-manifest.68c754c765a9a89c10555b4cc274ad91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43beaba76adfc87963a202d64ca66597",
+    "url": "/fb-meta-test/index.html"
+  },
+  {
+    "revision": "43a4033a0c32f2dd984a",
+    "url": "/fb-meta-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "75d40cd6885c2c3196eb",
+    "url": "/fb-meta-test/static/js/2.c1e1a3f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fb-meta-test/static/js/2.c1e1a3f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43a4033a0c32f2dd984a",
+    "url": "/fb-meta-test/static/js/main.4931fa6c.chunk.js"
+  },
+  {
+    "revision": "b43ee31ece1c66fc04ad",
+    "url": "/fb-meta-test/static/js/runtime-main.dfc26d59.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fb-meta-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
