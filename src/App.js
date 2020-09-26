55318831af6dc2986166aa69eed885495b5aa974
Karin Hsu <khsu0720@gmail.com>
2020-09-26 18:25:59 +0800

    g

--- src/App.js
+++ src/App.js
@@ -15,20 +15,20 @@ function App() {
 
 {/* <!-- Google / Search Engine Tags --> */}
 <meta itemprop="name" content="Lyric Keeper"/>
-<meta itemprop="description" content="A web & mobile app that allows you to easily keep track of all your lyrics!"/>
+<meta itemprop="description" content="el la google"/>
 <meta itemprop="image" content="https://images.unsplash.com/photo-1459749411175-04bf5292ceea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80"/>
 
 {/* <!-- Facebook Meta Tags --> */}
 <meta property="og:url" content="https://www.lyrickeeper.com"/>
 <meta property="og:type" content="website"/>
 <meta property="og:title" content="Lyric Keeper"/>
-<meta property="og:description" content="A web & mobile app that allows you to easily keep track of all your lyrics!"/>
+<meta property="og:description" content="Just an  FB APP "/>
 <meta property="og:image" content="https://images.unsplash.com/photo-1459749411175-04bf5292ceea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80"/>
 
 {/* <!-- Twitter Meta Tags --> */}
 <meta name="twitter:card" content="summary_large_image"/>
 <meta name="twitter:title" content="Lyric Keeper"/>
-<meta name="twitter:description" content="A web & mobile app that allows you to easily keep track of all your lyrics!"/>
+<meta name="twitter:description" content="just a twitter app "/>
 <meta name="twitter:image" content="https://images.unsplash.com/photo-1459749411175-04bf5292ceea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2250&q=80"/>
       </Helmet>
       <a
